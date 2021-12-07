fa9009fed2424a7b063bd1d292752c1e90e6b20d
Gilberto Superby <gilbertoliveir@gmail.com>
2021-12-07 08:44:10 -0300

    add

--- 2021/Elixir/advcode/test/day01_test.ex
+++ 2021/Elixir/advcode/test/day01_test.ex
@@ -1,7 +1,7 @@
 defmodule Day01Test do
   use ExUnit.Case
   doctest Day1
-  import Day1
+  # import Day1
 
   def input do
     [ "199", "200", "208", "210", "200", "207", "240", "269", "260", "263" ]
---
+++ 2021/Elixir/advcode/test/day02_test.ex
@@ -0,0 +1,22 @@
+defmodule Day02Test do
+  use ExUnit.Case
+  doctest Day2
+  import Day2
+
+  def input do
+    [ "forward 5",
+      "down 5",
+      "forward 8",
+      "up 3",
+      "down 8",
+      "forward 2" ]
+  end
+
+  test "part 1" do
+    assert part1(input()) == 150
+  end
+
+  test "part 2" do
+    assert part2(input()) == 900
+  end
+end
